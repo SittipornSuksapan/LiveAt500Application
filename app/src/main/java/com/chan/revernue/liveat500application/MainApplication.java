f38131d0b4a108060a6af0dd6468bde667258eda
DESKTOP-NE0NMGR\zzz <chanzaloveza@gmail.com>
2018-04-03 13:48:18 +0700

    Initial commit

---
+++ app/src/main/java/com/chan/revernue/liveat500application/MainApplication.java
@@ -0,0 +1,15 @@
+package com.chan.revernue.liveat500application;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/com/chan/revernue/liveat500application/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chan.revernue.liveat500application.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chan.revernue.liveat500application.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
